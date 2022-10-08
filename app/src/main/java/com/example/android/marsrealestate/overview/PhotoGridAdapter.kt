4e285f50060727897c314ccd9fd7a5e499faf4cc
nathan <nathanstickler@gmail.com>
2022-10-07 17:17:48 -0700

    Created an internal OnClickListener class in PhotoGridAdapter, added onClickListener paramater to photoGridAdapter class declaration, set up onClickListener() to pass marsProperty in onBindViewHolder() and updated PhotosGridAdapter binding to add click listener that passes the selected property

--- app/src/main/java/com/example/android/marsrealestate/overview/PhotoGridAdapter.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/PhotoGridAdapter.kt
@@ -18,6 +18,7 @@
 package com.example.android.marsrealestate.overview
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
@@ -25,7 +26,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.android.marsrealestate.databinding.GridViewItemBinding
 import com.example.android.marsrealestate.network.MarsProperty
 
-class PhotoGridAdapter : ListAdapter<MarsProperty, PhotoGridAdapter.MarsPropertyViewHolder>(DiffCallback) {
+class PhotoGridAdapter(val onClickListener: OnClickListener) : ListAdapter<MarsProperty, PhotoGridAdapter.MarsPropertyViewHolder>(DiffCallback) {
 
     class MarsPropertyViewHolder(private var binding: GridViewItemBinding):
         RecyclerView.ViewHolder(binding.root) {
@@ -51,6 +52,13 @@ class PhotoGridAdapter : ListAdapter<MarsProperty, PhotoGridAdapter.MarsProperty
 
     override fun onBindViewHolder(holder: MarsPropertyViewHolder, position: Int) {
         val marsProperty = getItem(position)
+        holder.itemView.setOnClickListener {
+            onClickListener.onClick(marsProperty)
+        }
         holder.bind(marsProperty)
+    }
+
+    class OnClickListener(val clickListener: (marsProperty: MarsProperty) -> Unit) {
+        fun onClick(marsProperty: MarsProperty) = clickListener(marsProperty)
     }
 }
